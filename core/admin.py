ffe387a5a6af04da6ad1f0dba705d317e3c75d76
FernandoNici <fernando.nici@gmail.com>
2020-04-24 23:33:34 -0300

    [curso python] criado table EVENTO

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import Evento
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_criacao', 'data_evento')
+    list_filter = ('titulo', 'usuario',)
+
+admin.site.register(Evento, EventoAdmin)
+
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Evento(models.Model):
+    class Meta:
+        db_table = 'evento'
+
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField()
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
